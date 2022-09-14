69cb10e6661d76da945455cb09f6bbb972f97f71
Vencevu <albertovencevu@gmail.com>
2022-09-14 16:41:37 +0200

    ok

--- GPC/js/practica1.js
+++ GPC/js/practica1.js
@@ -32,7 +32,7 @@ function init()
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.setClearColor( new THREE.Color(0xFFFFFF) );
-  document.getElementById('container').appendChild( renderer.domElement );
+  document.getElementById('canvas').appendChild( renderer.domElement );
 
   scene = new THREE.Scene();
 
